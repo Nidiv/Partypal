530bb0b2250c21e1cf72362f8523557d41afbd98
Nidiv Kayastha <nniiddiivv@gmail.com>
2024-12-22 12:02:46 +0545

    Inital master check

--- src/components/Sign-Up.vue
+++ src/components/Sign-Up.vue
@@ -2,8 +2,9 @@
   <div
     class="flex flex-col items-center space-y-4 p-6 bg-white font-zen rounded-md shadow-md w-full h-screen"
   >
-    <!-- Tailwind Check  -->
-    <div class="bg-red-500 text-white p-4">Tailwind CSS is working!</div>
+    <!-- Tailwind Check123  -->
+    <div class="bg-red-400 text-white p-4">Tailwind CSS is working!</div>
+    <div class="bg-red-400 text-white p-4">Tailwind CSS is 2</div>
     <h3 class="text-black font-bold">Sign Up</h3>
     <div class="bg-white w-2/5 p-6 rounded-md shadow-md">
       <!-- Full Name -->
